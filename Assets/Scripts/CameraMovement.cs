dd9d13708b24ba35722f6f9c5631d47be0ef9e35
Ash <afalzon12@gmail.com>
2023-04-03 12:57:59 +0200

    added debug mode for camera and optimized the collision detection
    not finished with this bloody detection but I need to eat

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -11,6 +11,7 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private float offset; // offset to make the camera a little be early so the user can see where they go
     [SerializeField] private LayerMask mask; // mask of what shoud bonk the camera
     [SerializeField] private float bonkSize; // size that should be bonked to
+    [SerializeField] private bool debugMode;
 
     private bool[] bonkDirection = new bool[4]; // saves the 4 directions of the player to know if they bonked a wall
     private float[] bonkDistance = new float[4]; // saves the 4 distances in the 4 directions
@@ -20,45 +21,19 @@ public class CameraMovement : MonoBehaviour
 
     private void Update()
     {
-        GatherInput();
-        DoesBonk(ref bonkDirection, ref bonkDistance, bonkSize);
-        Debug.DrawRay(target.position, Vector3.forward * bonkSize, Color.green);
-        Debug.DrawRay(target.position, Vector3.right * bonkSize, Color.green);
-        Debug.DrawRay(target.position, Vector3.left * bonkSize, Color.green);
-        Debug.DrawRay(target.position, Vector3.back * bonkSize, Color.green);
+        // game loop to gather everything we need
+        GatherInput(); // user input to calculate the offset
+        DoesBonk(ref bonkDirection, ref bonkDistance, bonkSize); // direction and distance from walls to stop moving camera
+
+        // check if we're in debug mode
+        if (debugMode) debugFunction();
     }
 
     // moves the camera smoothly with an offset so you see where you go
     private void LateUpdate()
     {
-        // this one is top right
-        if (bonkDirection[0])
-        {
-            Debug.Log("I bonk topright");
-            transform.position = Vector3.SmoothDamp(transform.position, target.position + input.ToIso().normalized * offset - Vector3.right * (bonkSize - bonkDistance[0]), ref currentVelocity, smoothTime);
-        }
-        // this one is top left
-        if (bonkDirection[1])
-        {
-            Debug.Log("I bonk topleft");
-            transform.position = Vector3.SmoothDamp(transform.position, target.position + input.ToIso().normalized * offset - Vector3.forward * (bonkSize - bonkDistance[1]), ref currentVelocity, smoothTime);
-        }
-        // this one is bottom left
-        if (bonkDirection[2])
-        {
-            Debug.Log("I bonk botleft");
-            transform.position = Vector3.SmoothDamp(transform.position, target.position + input.ToIso().normalized * offset - Vector3.left * (bonkSize - bonkDistance[2]), ref currentVelocity, smoothTime);
-        }
-        // this one is bottom right
-        if (bonkDirection[3])
-        {
-            Debug.Log("I bonk botright");
-            transform.position = Vector3.SmoothDamp(transform.position, target.position + input.ToIso().normalized * offset - Vector3.back * (bonkSize - bonkDistance[3]), ref currentVelocity, smoothTime);
-        }
-        else
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, target.position + input.ToIso().normalized * offset, ref currentVelocity, smoothTime); // move camera as normal
-        }
+        // game loop to do stuff based on the information we got from Update
+        MoveCamera();
     }
 
     // does the camera bonk a wall and in wich direction?
@@ -74,9 +49,56 @@ public class CameraMovement : MonoBehaviour
         bonkFloat[3] = hit3.distance;
     }
 
+    // makes the camera follow the player with a collision detection so it cannot hover over walls, holes and OOB in general
+    private void MoveCamera()
+    {
+        // if no collision move as normal
+        if (!bonkDirection[0] && !bonkDirection[1] && !bonkDirection[2] && !bonkDirection[3])
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, target.position + input.ToIso().normalized * offset, ref currentVelocity, smoothTime); // move camera as normal
+        }
+        else
+        {
+            // this one is top right
+            if (bonkDirection[0])
+            {
+                if(debugMode) Debug.Log("I bonk topright");
+                transform.position = Vector3.SmoothDamp(transform.position, target.position + input.ToIso().normalized * offset - Vector3.right * (bonkSize - bonkDistance[0]), ref currentVelocity, smoothTime);
+            }
+            // this one is top left
+            if (bonkDirection[1])
+            {
+                if (debugMode) Debug.Log("I bonk topleft");
+                transform.position = Vector3.SmoothDamp(transform.position, target.position + input.ToIso().normalized * offset - Vector3.forward * (bonkSize - bonkDistance[1]), ref currentVelocity, smoothTime);
+            }
+            // this one is bottom left
+            if (bonkDirection[2])
+            {
+                if (debugMode) Debug.Log("I bonk botleft");
+                transform.position = Vector3.SmoothDamp(transform.position, target.position + input.ToIso().normalized * offset - Vector3.left * (bonkSize - bonkDistance[2]), ref currentVelocity, smoothTime);
+            }
+            // this one is bottom right
+            if (bonkDirection[3])
+            {
+                if (debugMode) Debug.Log("I bonk botright");
+                transform.position = Vector3.SmoothDamp(transform.position, target.position + input.ToIso().normalized * offset - Vector3.back * (bonkSize - bonkDistance[3]), ref currentVelocity, smoothTime);
+            }
+        }
+    }
+
     // gets the input from whatever controller
     private void GatherInput()
     {
         input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")); // Raw makes it non analogical, wich is weird with a controller but removing it makes the keyboard controll laggy (look into that)
+    }
+
+    // debug mode function
+    private void debugFunction()
+    {
+        // draws rays that materialize player collision detection
+        Debug.DrawRay(target.position, Vector3.forward * bonkSize, Color.green);
+        Debug.DrawRay(target.position, Vector3.right * bonkSize, Color.green);
+        Debug.DrawRay(target.position, Vector3.left * bonkSize, Color.green);
+        Debug.DrawRay(target.position, Vector3.back * bonkSize, Color.green);
     }
 }
