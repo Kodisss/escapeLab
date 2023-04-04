4794e4b99f9835f0b635c433386469c13130c356
Ash <afalzon12@gmail.com>
2023-04-04 19:29:24 +0200

    fixed jumps
    Need to add the GroundCheck empty object to the script by drag and dropping it to the ground check transform

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
@@ -10,8 +12,10 @@ public class PlayerJump : MonoBehaviour
     // variables to jump
     [Header("Jumping Variables")]
     [SerializeField] private float jumpForce = 6f;
+
+    [Header("Ground Checking Stuffs")]
     [SerializeField] private LayerMask groundMask;
-    private bool isGrounded;
+    [SerializeField] private Transform groundCheck;
 
     // debug mode
     [Header("")]
@@ -31,27 +35,26 @@ public class PlayerJump : MonoBehaviour
 
     private void FixedUpdate()
     {
-        CheckGrounded();
+        //CheckGrounded();
     }
 
     // checks if the player is at 1.1 distance of the ground
-    private void CheckGrounded()
+    private bool CheckGrounded()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f, groundMask);
-        if (debugMode)
-        {
-            Debug.DrawRay(rb.position, Vector3.down * 1.1f, Color.red);
-            if (isGrounded) Debug.Log("Am Grounded");
-        }
+        return Physics.CheckSphere(groundCheck.position, 0.1f, groundMask);
     }
 
     // check if the jump button is pressed to jump by addind a force
     private void Jump()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && CheckGrounded())
         {
             // rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             rb.velocity = Vector3.up * jumpForce;
+        }
+        if (debugMode)
+        {
+            if (CheckGrounded()) Debug.Log("Am Grounded");
         }
     }
 }
